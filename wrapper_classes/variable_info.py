135f2dd30c44fcd70d47d7388ea81d4a1ae9c123
Ratilife <ratilife81@gmail.com>
2025-09-23 12:21:58 +0300

    класс обертка для переменных

---
+++ wrapper_classes/variable_info.py
@@ -0,0 +1,41 @@
+from typing import List, Dict, Any, Optional, Set
+
+class VariableInfo:
+    def __init__(self, name):
+        self.name = name
+        self.type = None
+        self.type_annotation = None  # статические аннотации типов
+        self.scope = None  # 'global', 'local', 'class', 'instance'
+        self.access_modifier = None  # 'public', 'protected', 'private'
+        
+        # Жизненный цикл
+        self.declaration_location = None  # (file, line, module)
+        self.initialization_location = None  # (file, line, module)
+        self.usage_scope = []  # области где используется
+        self.lifetime_info = {}
+        
+        # Использование
+        self.usage_count = 0
+        self.operations = []  # список операций
+        self.usage_pattern = None  # 'counter', 'flag', 'buffer', etc.
+        self.dependencies = []  # локальные зависимости
+        
+        # Межмодульные связи
+        self.imported_from = None  # если переменная импортирована
+        self.exported_to = []  # модули, куда экспортируется
+        self.cross_module_references = []  # ссылки из других модулей
+        
+        # Значения
+        self.initial_value = None
+        self.value_range = None
+        self.is_constant = False
+        self.value_history = []
+        
+        # Декораторы и аннотации
+        self.decorators = []  # декораторы для переменных (например, @property)
+        self.annotations = {}  # дополнительные аннотации
+        
+        # Связи
+        self.related_variables = []  # связанные переменные
+        self.parent_variable = None  # для наследования
+        self.overrides = None  # если переопределяет переменную родителя
